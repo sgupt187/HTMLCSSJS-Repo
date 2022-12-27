bca4f98dec27805488d2ebde8e8de7cf1748c62f
Shalu-star <shalukris7@gmail.com>
2022-12-27 14:56:20 +0530

    module4 solution

---
+++ module4-solution/js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+    var names = ["Naina", "Misti", "John", "Jerry", "Sonali", "Akanksha", "Vaishnavi", "Atul", "Utsav", "Abhishek"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
