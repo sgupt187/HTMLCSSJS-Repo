e6c1ae85658db634e862302138c1c5ef20a4b5dd
Shalu-star <shalukris7@gmail.com>
2023-01-04 17:27:18 +0530

    module5 solution

---
+++ module5-solution/js/ajax-utils.js
@@ -0,0 +1,20 @@
+(function (global) {
+
+// Set up a namespace for our utility
+var ajaxUtils = {};
+
+
+// Returns an HTTP request object
+function getRequestObject() {
+  if (window.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else if (window.ActiveXObject) {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
+  }
+}
